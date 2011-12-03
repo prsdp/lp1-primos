e19b372c20d99606f26b0529f2d2f3467711cbc5
Priscila Paixão <prsdp18@gmail.com>
2011-12-03 15:10:20 -0400

    Main: criado o arquivo principal
    Signed-off-by: Priscila Paixão <prsdp18@gmail.com>

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Descrição dos números primos.
+ *
+ * Esse trabalho é uma imlementação do
+ * trabalho final do 1 período da
+ * matéria de Linguagem de Programação
+ * do curso de Engenharia da Computação
+ * da Faculdade FUCAPI 2011.
+ *
+ * @author Priscila Paixão
+ *
+ */
+
+int main (int argc, char **argv){
+
+return 0;
+
+}
