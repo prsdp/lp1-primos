38ee752cc6457e64d3b95edb756f5072bf7c3edc
Jônatas Pedraza <jonatas.nona@gmail.com>
2011-12-05 13:37:36 -0400

    Intervals: troquei o nome da lib
    time_intervals.h agora se chama intervals.h
    e declara o procedimento de impressão dos
    intervalos dos primos chamado print_intervals(int li, int ls)
    
    Signed-off-by: Jônatas Pedraza <jonatas.nona@gmail.com>

--- intervals.h
+++ intervals.h
@@ -7,6 +7,28 @@
  *
  */
 
+// procedimento para imprimir os intervalos
+void print_interval(int li, int ls){
+
+    int i,j,cont=0;
+    int *vetor;
+
+    vetor = interval_primes(li, ls, &cont);
+
+    printf("\n Intervalo [%d,%d]  = {",li,ls);
+
+    for(i=0;i<cont;i++){
+        if (i == cont - 1)
+            printf("%d", vetor[i]);
+        else
+            printf("%d,",vetor[i]);
+
+    }
+
+    printf("}");
+    free(vetor);
+}
+
 // Esta função tem como parâmetros o limite
 // inferior e superior dos intervalos e
 // retorna o tempo de execução do intervalo.
