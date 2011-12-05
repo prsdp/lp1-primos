41335bbd100d773d36a538c2234baa835d68b39e
Jônatas Pedraza <jonatas.nona@gmail.com>
2011-12-05 13:39:12 -0400

    Main: Adicionado lib intervals.h
    só troquei o nome da biblioteca como dito
    no commit anterior e removi a declaração
    de print_intervals do arquivo main.c
    
    Signed-off-by: Jônatas Pedraza <jonatas.nona@gmail.com>

--- main.c
+++ main.c
@@ -15,28 +15,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "primos.h"
-#include "time_intervals.h"
-
-void print_interval(int li, int ls){
-
-    int i,j,cont=0;
-    int *vetor;
-
-    vetor = interval_primes(li, ls, &cont);
-
-    printf("\n Intervalo [%d,%d]  = {",li,ls);
-
-    for(i=0;i<cont;i++){
-        if (i == cont - 1)
-            printf("%d", vetor[i]);
-        else
-            printf("%d,",vetor[i]);
-
-    }
-
-    printf("}");
-    free(vetor);
-}
+#include "intervals.h"
 
 int main (int argc, char **argv){
 
@@ -60,8 +39,6 @@ int main (int argc, char **argv){
     printf ("\t %fs",get_time_interval(1,90));
     print_interval(1,100);
     printf ("\t %fs",get_time_interval(1,100));
-
-    printf ("\t %fs",get_time_interval(1,20000));
 
     printf("\n\n");
 
